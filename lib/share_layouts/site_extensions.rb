b125a05f1021ad7b12b5aca9c2d96307f9d952e0
spanner <will@grytpype-3.home>
2010-06-08 15:14:38 +0100

    sites given default_layout

--- lib/share_layouts/site_extensions.rb
+++ lib/share_layouts/site_extensions.rb
@@ -1,7 +1,12 @@
 module ShareLayouts::SiteExtensions
 
+  def self.included(base)
+    base.class_eval {
+      belongs_to :default_layout, :class_name => "Layout"
+    }
+  end
+
   def layout_for(area = :public)
-    default = Radiant::Config["#{area}.layout"]
     if self.respond_to?("#{area}_layout") && associated_layout = self.send("#{area}_layout".intern)
       associated_layout.name
     elsif configured_layout = Layout.find_by_name(Radiant::Config["#{area}.layout"])
@@ -10,8 +15,8 @@ module ShareLayouts::SiteExtensions
       named_layout.name
     elsif default_layout = self.default_layout
       default_layout.name
-    else
-      Layout.first.name
+    elsif any_layout = Layout.first
+      any_layout.name
     end
   end
 
